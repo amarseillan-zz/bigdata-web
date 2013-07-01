d890fb141d5e5adb79ca36155c111b4a0ec9adce
Agustin Marseillan <agustinmarseillan@gmail.com>
2013-07-01 16:20:03 -0300

    readme y configurable intervalo

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 bigdata-web
 ===========
+
+Bigdata visualization project
+
+Created by:
+	Martín Juan Sturla
+	Agustín Marseillan
+	María Victoria Mesa Alcorta
+
+To run:
+	While in eclipse, run Class src/test/java:bigdata.web.RunBigdata.java
+	or
+	>./make #and use the generated .war with your favourite web server application
+
+	Want to see the deployed version?
+	Add "54.224.21.206 10.212.83.136 hadoop-2013-datanode-6" to your /etc/hosts and visit http://hadoop-2013-datanode-3:8080/bigdata-web/
+Web context: 	/bigdata/ for jetty
+		/bigdata-web/ for tomcat
+To navigate: The page is self-explanatory
--- src/main/java/bigdata/web/controller/IndexController.java
+++ src/main/java/bigdata/web/controller/IndexController.java
@@ -3,6 +3,7 @@ package bigdata.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,8 +11,9 @@ import org.springframework.web.servlet.ModelAndView;
 public class IndexController {
 
 	@RequestMapping(value = "index", method = RequestMethod.GET)
-	protected ModelAndView index() {
+	protected ModelAndView index(@RequestParam( required = false ) Long interval) {
 		ModelAndView mav = new ModelAndView("index");
+		mav.addObject("interval", interval);
 		return mav;
 	}
 
